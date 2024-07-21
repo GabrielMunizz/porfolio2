e15e4480ba448c42e9380fd68b534d93aa870582
Muniz <gabrielmfd@gmail.com>
2024-07-21 12:05:10 -0300

    Feat: adiciona animação de scroll na entrada dos containers na tela

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
+import { useScroll, motion } from "framer-motion";
 
 export default function Container({ children }: { children: React.ReactNode }) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "start start"],
+  });
   return (
-    <div className="flex flex-col justify-center items-center min-w-full min-h-[100vh]">
+    <motion.div
+      ref={containerRef}
+      style={{ scale: scrollYProgress, opacity: scrollYProgress }}
+      className="flex flex-col justify-center items-center min-w-full min-h-[100vh]"
+    >
       <div className="flex flex-row justify-around items-center border-2 border-white w-[80%] h-[85vh] mt-[10vh] shadow-[4px_4px_0px_white] rounded-lg">
         {children}
       </div>
-    </div>
+    </motion.div>
   );
 }
