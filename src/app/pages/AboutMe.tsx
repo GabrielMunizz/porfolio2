0a439a63d3f2c6debb3720db9784d0e128c74eef
Muniz <gabrielmfd@gmail.com>
2024-10-16 09:21:39 -0300

    Altera overlay da foto de perfil no AboutMe

--- src/app/pages/AboutMe.tsx
+++ src/app/pages/AboutMe.tsx
@@ -24,7 +24,7 @@ export default function AboutMe() {
           alt="Gabriel's picture"
           
         />
-        <div className="hidden xl:block absolute inset-0 bg-gradient-to-r from-black via-black/10 2xl:via-black/60 to-transparent z-[15] xl:mt-[1px]" />
+        <div className="hidden xl:block absolute left-0 w-[50%] h-full bg-gradient-to-r from-black via-black 2xl:via-black/80 to-transparent z-[15] xl:mt-[1px]" />
       </div>
       <Image
         className="absolute rounded-[50%] w-[7rem] h-[7rem] z-10 object-cover right-[5%] top-4 md:right-10 md:top-5 md:w-[15rem] md:h-[15rem] xl:hidden"
